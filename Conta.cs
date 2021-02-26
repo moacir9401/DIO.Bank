0221827013797be1171987d19ec5418c9beb3265
Moacir <moacir9401@gmail.com>
2021-02-26 10:57:20 -0300

    criação do projeto, conta e enum

---
+++ Conta.cs
@@ -0,0 +1,16 @@
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        private TipoConta tipoConta { get; set; }
+        private double Credito { get; set; }
+        private double Saldo { get; set; }
+        private string Nome { get; set; }
+
+    public Conta()
+    {
+        
+    }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta();
+            minhaConta.Nome = "Moacir";
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
